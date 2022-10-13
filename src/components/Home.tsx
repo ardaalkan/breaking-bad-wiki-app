39f93c6098cd5db9d6eafa3192892f746e787383
ardaalkan <ardaaa.alk@gmail.com>
2022-10-13 18:06:54 +0300

    added filter funcs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,13 +2,13 @@ import logo from "../logo.png";
 
 const Home = () => {
   return (
-    <div className="m-24 bg-slate-300 p-6 rounded-sm">
+    <div className="m-24 bg-indigo-500 p-6 rounded-md">
       <img src={logo} alt="logo" />
       <div className="mt-12">
-        <span className="font-bold text-gray-900 text-base">
+        <span className="font-bold text-gray-100 text-base">
           A wiki application that contains the characters, details and pictures
           of the legendary series{" "}
-          <span className="text-green-800">Breaking Bad.</span>
+          <span className="text-green-400">Breaking Bad.</span>
         </span>
         <span className="ml-6 bg-green-700 text-white p-3 rounded-full hover:bg-green-600 duration-300">
           <a href="https://www.breakingbadapi.com">Breaking Bad API</a>
--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,11 +1,20 @@
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { API_URL_QUOTES } from "../API";
 import QuotesCard from "./QuotesCard";
+import React from "react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-const Quotes = () => {
-  const { data, error } = useSWR(API_URL_QUOTES, fetcher);
+export default function Quotes() {
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <QuotesFunc />
+    </SWRConfig>
+  );
+}
+
+function QuotesFunc() {
+  const { data, error } = useSWR(API_URL_QUOTES);
 
   if (error) {
     return <div className="font-bold text-gray-700 text-base">Error...</div>;
@@ -18,14 +27,50 @@ const Quotes = () => {
   console.log(data);
   //TODO: Name Filter
   return (
+    <DisplayQuotesFunc
+      data={data}
+      quotes={[...new Set(data.map((quotes: any) => quotes.author))]}
+    />
+  );
+}
+
+function DisplayQuotesFunc({ data, quotes }: any) {
+  const [filterQuote, setFilterQuote] = React.useState(null);
+
+  const filteredQuote = filterQuote
+    ? data.filter((datas: any) => datas.quote)
+    : data;
+
+  return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 gap-6 mt-6 mb-6">
-        {data.map((data: any) => (
-          <QuotesCard {...data} />
-        ))}
+        <>
+          {quotes.map((quote: any) => (
+            <button
+              className="p-1 bg-slate-300"
+              onClick={() => {
+                setFilterQuote(quote);
+              }}
+              key={quote}
+            >
+              {quote}
+            </button>
+          ))}
+          {filterQuote && (
+            <button
+            className="p-1 bg-slate-200"
+              onClick={() => {
+                setFilterQuote(null);
+              }}
+            >
+              RESET
+            </button>
+          )}
+          {filteredQuote.map((data: any) => (
+            <QuotesCard {...data} />
+          ))}
+        </>
       </div>
     </div>
   );
-};
-
-export default Quotes;
+}
